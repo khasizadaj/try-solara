d591d3e5bb505f7a60391458d13e093683a319d6
khasizadaj <javid.khasizada@gmail.com>
2024-10-25 22:50:35 +0200

    Add laoding mechanism
    - Data is loaded only if button is clicked.
    - Only some portion of data is shown for readability
    - Full dataset can be downloaded with download button.
    
    Issues: CSV file is automaticaly generated for download. It should be triggered after click.

--- sol.py
+++ sol.py
@@ -2,13 +2,16 @@ import uuid
 import pandas as pd
 import solara
 from solara import (
+    Button,
     Column,
     DataFrame,
     FileDownload,
+    Info,
     Path,
     Select,
     Markdown,
     Sidebar,
+    Success,
 )
 
 LOG_IDS = [
@@ -41,6 +44,19 @@ event_log_id = solara.reactive(LOG_IDS[0])
 customer = solara.reactive(CUSTOMERS[0])
 
 df = pd.read_csv("generated_log_data.csv")
+shown_data = solara.reactive(None)
+saved_data = solara.reactive(None)
+summary_label = solara.reactive("")
+
+
+def show_data():
+    global df
+    result = df[
+        (df["Customer"] == customer.value) & (df["LogId"] == event_log_id.value)
+    ]
+    saved_data.value = result
+    shown_data.value = result.head(10)
+    summary_label.value = f"Parsed logs for {customer.value} with {event_log_id.value}. Whole dataset has {saved_data.value.shape[0]} event logs."
 
 
 @solara.component
@@ -53,18 +69,19 @@ def Page():
     with Sidebar():
         Select(label="Customer", value=customer, values=CUSTOMERS)
         Select(label="Event Log Identifier", value=event_log_id, values=LOG_IDS)
+        Button(label="Get data", on_click=show_data)
+        Info("This is only gonna be sample data and first 10 event logs will be shown.", dense=True, classes=["my-3"])
 
     with Column(classes=["container"]):
-        Markdown("## Summary")
-        Markdown(f"Parsed logs for {customer.value} with {event_log_id.value}")
-        global df
-        local_df = df[
-            (df["Customer"] == customer.value) & (df["LogId"] == event_log_id.value)
-        ]
+        Markdown("# Summary")
 
-        DataFrame(local_df, items_per_page=10)
-        FileDownload(
-            local_df.reset_index(drop=True).to_csv(index=False),
-            filename=f"{str(uuid.uuid5(name=f'{customer.value}+{event_log_id.value}', namespace=uuid.NAMESPACE_URL))}.csv",
-            label="Download file",
-        )
+        if shown_data.value is not None:
+            Success(summary_label.value)
+            DataFrame(shown_data.value.reset_index(drop=True), items_per_page=10, scrollable=False)
+            FileDownload(
+                saved_data.value.to_csv(index=False),
+                filename=f"{str(uuid.uuid5(name=f'{customer.value}+{event_log_id.value}', namespace=uuid.NAMESPACE_URL))}.csv",
+                label=f"Download file: {saved_data.value.shape[0]} events",
+            )
+
+        
